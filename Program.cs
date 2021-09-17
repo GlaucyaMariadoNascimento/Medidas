ea385f17a8fafc2bec4a1021fdc0d08602e0d4a5
Savioli Pri <stobbeglaucya@gmail.com>
2021-09-17 19:37:16 -0300

    Adicionar resolução exercício Medida

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+          double metros, metrosparacm, metrosparakm;
+          Console.Write("Entre com a medida em metros: ");
+          metros = Convert.ToDouble(Console.ReadLine());
+        
+          Console.WriteLine("\n----Equivalencia----");
+
+          metrosparacm = metros*100;
+
+          metrosparakm = metros/1000;
+
+          Console.WriteLine($"{metrosparacm} cm");
+
+          Console.WriteLine($"{metros} m");
+
+          Console.WriteLine($"{metrosparakm} km");
+            
+        }
+    }
+}
